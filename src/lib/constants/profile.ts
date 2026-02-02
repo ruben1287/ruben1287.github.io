5cc48505ac07aee489c2250c497257db162b169b
Ruben <rubenramirez1287@gmail.com>
2026-02-02 18:53:52 +0000

    change profile

--- src/lib/constants/profile.ts
+++ src/lib/constants/profile.ts
@@ -1,12 +1,11 @@
 export const PROFILE_INFO = {
-  role: 'Software Engineer / Full-stack Developer',
-  logo: 'Louai.b',
-  slogan: 'Coding, Creativity, Coffee...',
-  displayName: "Louai Boumediene",
-  email: "boumedienelouai2@gmail.com",
+  role: 'Devops Engineer',
+  logo: 'Ruebn R',
+  //slogan: 'Coding, Creativity, Coffee...',
+  displayName: "Ruben Ramirez",
+  email: "rubenramirez1287@gmail.com",
   about: `
-I'm a **software engineer** passionate about crafting high-quality digital experiences. With expertise in **full-stack** development and a strong eye for **UI/UX**, I build **performant, pixel-perfect** applications that are both functional and visually appealing.  
-Always **learning**, always **iterating**, always pushing boundaries.  
+I'm a **devops engineer** ....  
 `,
 } as const;
 
