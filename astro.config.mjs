d967c81a4e6a68592471c70276c0de8cf913ddea
ruben1287 <59667692+ruben1287@users.noreply.github.com>
2026-02-02 12:04:07 -0300

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://louai.site',
+  site: 'https://ruben1287.github.io',
   integrations: [react(), mdx()],
   vite: {
     plugins: [tailwindcss()],
